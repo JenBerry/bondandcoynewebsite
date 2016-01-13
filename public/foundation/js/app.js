c91b04ef4a982d31b292a0723326710b275beb87
Jen Berry <jenberrymail@gmail.com>
2016-01-13 09:58:08 +0000

    work page - fade and animate 'read more'

--- public/foundation/js/app.js
+++ public/foundation/js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+
+
+function showToggle($container,$button){
+	if ($container.hasClass('truncated')){
+		$button.html('<i class="fi-minus alt-color"></i> Read Less');
+		$contentHeight = $container.children($('.trucateContent')).height();
+		$container.css('height',$contentHeight);
+	}
+	else{
+		$button.html('<i class="fi-plus alt-color"></i> Read More');
+	}
+
+	$container.toggleClass('truncated');
+}
